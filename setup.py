34b599f7ded29ddd0771c7de097b269d2df98e94
Karl Heggenberger <karl.heggenberger@gmx.de>
2023-10-13 11:44:36 +0200

    prepare pip package

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup, find_packages
+import os
 
 with open("requirements.txt", "r") as requirements_file:
     install_requires = requirements_file.read().splitlines()
 
+def read_readme():
+    readme_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
+    with open(readme_file) as f:
+        readme = f.read()
+    return readme
+
 setup(
-    name = 'autodoc',
+    name = 'autodoc_pyfiles',
     version = '0.1',
+    description='Automated documentationstring generation for python files within repositories, folders or for sinlge .py files.',
+    long_description=read_readme(),
+    url='https://github.com/StructuralNeurobiologyLab/autodoc',
+    download_url='https://github.com/StructuralNeurobiologyLab/autodoc',
+    author='Karl Heggenberger, Joergen Kornfeld.',
+    author_email='karl.heggenberger@bi.mpg.de',
     packages = find_packages(),
     install_requires = install_requires,
     entry_points = {
